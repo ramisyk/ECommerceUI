2e8b391388ceebfb60209dd61922624128d57881
ramisyk <ramisyk97@gmail.com>
2023-07-06 15:37:47 +0300

    delete dialog grated

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,6 +1,9 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { async } from 'rxjs';
 import { SpinnerType } from '../../base/base.component';
+import { DeleteDialogComponent, DeleteState } from '../../dialogs/delete-dialog/delete-dialog.component';
 import { ProductService } from '../../services/common/models/product.service';
 
 declare var $: any;
@@ -14,7 +17,8 @@ export class DeleteDirective {
     private element: ElementRef,
     private _renderer: Renderer2,
     private productService: ProductService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    public dialog: MatDialog
   ) {
     const img = _renderer.createElement("img");
     img.setAttribute("src", "../../../../../assets/delete.png");
@@ -29,11 +33,33 @@ export class DeleteDirective {
 
   @HostListener("click")
   async onclick() {
-    this.spinner.show(SpinnerType.BallAtom);
-    const td: HTMLTableCellElement = this.element.nativeElement;
-    await this.productService.delete(this.id)
-    $(td.parentElement).fadeOut(2000, () => {
-      this.callback.emit();
+    this.openDialog(async () => {
+      this.spinner.show(SpinnerType.BallAtom);
+      const td: HTMLTableCellElement = this.element.nativeElement;
+      await this.productService.delete(this.id);
+      $(td.parentElement).animate({
+        opacity: 0,
+        left: "+=50",
+        height: "toogle"
+      }, 700, () => {
+        this.callback.emit();
+      });
+    });
+    
+  }
+
+  openDialog(afterClosed: any): void {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width: '250px',
+      data: DeleteState.Yes,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == DeleteState.Yes) {
+        afterClosed()
+      }
     });
   }
+
+
 }
