d301f66a6374e4d8a453216199544d77022baa69
ramisyk <ramisyk97@gmail.com>
2023-07-31 14:46:17 +0300

    Global Http Error Handler created with Http Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,12 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from "ngx-spinner";
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
 import { LoginComponent } from './ui/components/login/login.component';
 import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
 import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
+import { HttpErrorHandlerInterceptorService } from './services/common/http-error-handler-interceptor.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -60,7 +61,8 @@ import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
           console.log("hata alındı...")
         }
       } as SocialAuthServiceConfig
-    }
+    },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorHandlerInterceptorService, multi: true }
   ],
   bootstrap: [AppComponent]
 })
