bff41c131c75dc3a291a999e8f5f6228a3f23901
Ramis Yüksel <ramisyk97@gmail.com>
2023-09-28 08:08:59 +0300

    signalr tested in dashboard

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent } from '../../../base/base.component';
 import { SignalRService } from 'src/app/services/common/signalr.service';
-import { AlertifyService } from 'src/app/services/admin/alertify.service';
+import { AlertifyMessagePosition, AlertifyMessageType, AlertifyService } from 'src/app/services/admin/alertify.service';
 import { ReceiveFunctions } from 'src/app/constants/receive-functions';
 import { HubUrls } from 'src/app/constants/hub-urls';
 
@@ -21,7 +21,10 @@ export class DashboardComponent extends BaseComponent implements OnInit {
   ngOnInit() {
     this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
       this.alertifyService.message(message, {
-        delay:1
+        delay: 3,
+        dismissOthers: true,
+        messageType: AlertifyMessageType.Notify,
+        position: AlertifyMessagePosition.TopRight
       });
     });
   }
